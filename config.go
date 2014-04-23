f2f045dd4881238b649ef961a0c2c8d70b0bd283
Takashi Nakagawa <tak1240@gmail.com>
2014-04-23 16:54:54 +0900

    add config struct and RemoteConfig struct to config.go

--- config.go
+++ config.go
@@ -1,10 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
 
-func LoadConfig() error {
+type RemoteConfig struct {
+	Host     string
+	Repo     string
+	Api      string
+	ApiToken string `json: "api_token"`
+}
+
+type Config struct {
+	Dirs   []string
+	Files  []string
+	Format string
+	Remote RemoteConfig `json: "remote_config"`
+	Ignore []string
+}
+
+func LoadConfig() (Config, error) {
+	var c Config
+	// raise error if erkconfig.json is nor found in current directory.
 	if !CheckFileExistence(CONF_FILENAME) {
-		return fmt.Errorf("Configuration file: %s is not found. please run \"erk init\" first.", CONF_FILENAME)
+		return c, fmt.Errorf("Configuration file: %s is not found. please run \"erk init\" first.", CONF_FILENAME)
 	}
-	return nil
+
+	// read json config file.
+	body, err := ioutil.ReadFile(CONF_FILENAME)
+	if err != nil {
+		return c, err
+	}
+
+	// parse json and put them into Config struct.
+	err = json.Unmarshal(body, &c)
+	if err != nil {
+		return c, err
+	}
+	return c, nil
 }
