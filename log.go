c3f54fe2475ede21036461597ef29967f145a28b
Takashi Nakagawa <tak1240@gmail.com>
2014-04-23 09:36:06 +0900

    delete unused log functions

--- log.go
+++ log.go
@@ -10,25 +10,9 @@ const (
 	errorLogFormat   = "\033[31merk: %s\033[0m"
 )
 
-var logFormat = ""
-
-func formatLog(msg string) string {
-	if logFormat == "" {
-		logFormat = os.Getenv("DIRENV_LOG_FORMAT")
-		if logFormat == "" {
-			logFormat = defaultLogFormat
-		}
-	}
-	return fmt.Sprintf(logFormat, msg)
-}
-
 func log_error(msg string, a ...interface{}) {
 	msg = fmt.Sprintf(errorLogFormat, msg)
 	log(msg, a...)
-}
-
-func log_status(msg string, a ...interface{}) {
-	log(formatLog(msg), a...)
 }
 
 func log(msg string, a ...interface{}) {
