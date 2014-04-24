76cce60fd0d5d00fa232f764a845f767035936e7
Takashi Nakagawa <tak1240@gmail.com>
2014-04-24 19:51:56 +0900

    use current wokring directory for basePath

--- cmd_run.go
+++ cmd_run.go
@@ -1,11 +1,16 @@
 package main
 
+import "os"
+
 var CmdRun = &Cmd{
 	Name: "run",
 	Desc: "run erk to search inline issue.",
 	Fn: func(args []string) error {
-		basePath := "/Users/tak0303/src/beatrobo/PlugAir-Manage-API/"
-		err := ParseFiles(basePath)
+		basePath, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		err = ParseFiles(basePath)
 		if err != nil {
 			return err
 		}
