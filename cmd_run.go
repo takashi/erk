d3face5987e02f94e7c2102dea2776140bfb8725
Takashi Nakagawa <tak1240@gmail.com>
2014-04-24 18:15:18 +0900

    create Fn
    basePath is just for a test, in production, basepath generate
    automatically from current directory

--- cmd_run.go
+++ cmd_run.go
@@ -3,8 +3,12 @@ package main
 var CmdRun = &Cmd{
 	Name: "update",
 	Desc: "run erk to search inline issue.",
-	Fn: func(args []string, config Config) error {
-		// noob
+	Fn: func(args []string) error {
+		basePath := "/Users/tak0303/src/beatrobo/PlugAir-Manage-API/"
+		err := ParseFiles(basePath)
+		if err != nil {
+			return err
+		}
 		return nil
 	},
 }
