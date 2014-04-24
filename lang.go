afde27bb3d91f4ab589de579e824c684bd137444
Takashi Nakagawa <tak1240@gmail.com>
2014-04-24 12:30:04 +0900

    define Lang struct for define supprted languages

---
+++ lang.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type Lang struct {
+	Name             string
+	Ext              string
+	OneLineComment   string
+	MultiLineComment string
+}
+
+var LangList []*Lang
+
+func init() {
+	LangList = []*Lang{}
+}
+
+func DetectLangFromExt(ext string) (*Lang, error) {
+	var lang *Lang
+
+	for _, l := range LangList {
+		if ext == l.Ext {
+			lang = l
+			break
+		}
+	}
+
+	if lang == nil {
+		return nil, fmt.Errorf("The file which has \"%s\" extension is not supported.", ext)
+	}
+	return lang, nil
+}
