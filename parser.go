c9dc83f07aa93e98ca123dcdb6b0b91581ebd554
Takashi Nakagawa <tak1240@gmail.com>
2014-04-24 19:21:03 +0900

    add parseIssues function (wip)

--- parser.go
+++ parser.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
 )
 
 func ParseFiles(dir string) error {
@@ -26,8 +29,23 @@ func WalkFn(path string, info os.FileInfo, err error) error {
 	} else if info.IsDir() {
 		// do nothing
 	} else {
-		// do the file parse here
-		println(info.Name())
+		ParseIssues(path string, info os.FileInfo)
 	}
 	return nil
+}
+
+func ParseIssues() {
+	// do the file parse here
+	lang := DetectLangFromExt(filepath.Ext(path))
+	if lang != nil {
+		body, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		re, _ := regexp.Compile(`# \[TODO\].*`)
+		one := re.Find(body)
+		if one != nil {
+			fmt.Printf("Find a TODO: %s \n    at %s\n", one, path)
+		}
+	}
 }
