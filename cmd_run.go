68828e2a9f1936cee8bf1a0c9ba5c45c09bbb695
Takashi Nakagawa <tak1240@gmail.com>
2014-04-28 19:05:40 +0900

    use relative path

--- cmd_run.go
+++ cmd_run.go
@@ -1,18 +1,24 @@
 package main
 
-import "os"
+import "fmt"
 
 var CmdRun = &Cmd{
 	Name: "run",
 	Desc: "run erk to search inline issue.",
 	Fn: func(args []string) error {
-		basePath, err := os.Getwd()
+		// basePath, err := os.Getwd()
+		// if err != nil {
+		// 	return err
+		// }
+		err := ParseFiles( /*basePath*/)
 		if err != nil {
 			return err
 		}
-		err = ParseFiles(basePath)
-		if err != nil {
-			return err
+		for _, issue := range IssueList {
+			fmt.Printf(`[%s] at %s
+  line %d - %s %s
+
+`, issue.Label, issue.FileName, issue.Line, issue.Title, issue.FilePath)
 		}
 		return nil
 	},
