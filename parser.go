0bdd3ca917876660cde4b2fd5b1a37e730df1232
Takashi Nakagawa <tak1240@gmail.com>
2014-04-28 19:05:58 +0900

    add parsed issue to IssueList slice

--- parser.go
+++ parser.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"bufio"
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 )
 
-func ParseFiles(dir string) error {
-	err := filepath.Walk(dir, WalkFn)
+func ParseFiles( /*dir string*/) error {
+	err := filepath.Walk("./", WalkFn)
 	if err != nil {
 		return err
 	}
@@ -41,15 +41,39 @@ func ParseIssues(path string, info os.FileInfo) error {
 	// do the file parse here
 	lang := DetectLangFromExt(filepath.Ext(path))
 	if lang != nil {
-		body, err := ioutil.ReadFile(path)
+		err := ParseIssuesByLang(lang, path, info)
 		if err != nil {
 			return err
 		}
-		re, _ := regexp.Compile(lang.OneLineComment + ` \[TODO\].*`)
-		one := re.Find(body)
-		if one != nil {
-			fmt.Printf("Find a TODO: %s \n    at %s\n", one, path)
+	}
+	return nil
+}
+
+func ParseIssuesByLang(lang *Lang, path string, info os.FileInfo) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			panic(err)
 		}
+	}()
+	scanner := bufio.NewScanner(file)
+	line := 1
+	re, _ := regexp.Compile(lang.OneLineComment + `.*\[(?i)` + config.Label + `\].*`)
+	commnentLabelRe, _ := regexp.Compile(lang.OneLineComment + `.*\[(?i)` + config.Label + `\].*\-`)
+	for scanner.Scan() {
+		one := re.Find([]byte(scanner.Text()))
+		if one != nil {
+			title := strings.TrimPrefix(string(commnentLabelRe.ReplaceAll(one, []byte(""))), " ")
+			issue := &Issue{Title: title, FileName: info.Name(), Label: config.Label, Line: line, FilePath: path}
+			IssueList = append(IssueList, issue)
+		}
+		line += 1
+	}
+	if err := scanner.Err(); err != nil {
+		return err
 	}
 	return nil
 }
