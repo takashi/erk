d83834d826def912a4f5b0efbaf5760e3623d2d0
Takashi Nakagawa <tak1240@gmail.com>
2014-05-07 21:32:45 +0900

    create remote issue (tmp)

--- adapter_github.go
+++ adapter_github.go
@@ -16,12 +16,12 @@ func (d *AdapterGithub) Update() error {
 	}
 	client := github.NewClient(t.Client())
 	repoInfo := strings.Split(config.RemoteConfig.Repo, "/")
-	issues, _, err := client.Issues.ListByRepo(repoInfo[0], repoInfo[1], nil)
-	if err != nil {
-		return err
-	}
-	for _, issue := range issues {
-		println(*issue.Title)
+	// remoteIssues, _, err := client.Issues.ListByRepo(repoInfo[0], repoInfo[1], nil)
+	// if err != nil {
+	// 	return err
+	// }
+	for _, issue := range IssueList {
+		client.Issues.Create(repoInfo[0], repoInfo[1], &github.IssueRequest{Title: &issue.Title})
 	}
 	return nil
 }
